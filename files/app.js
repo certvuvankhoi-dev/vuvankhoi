b93daf8a59b4b218e088a62ccb355b84b7df03cd
certvuvankhoi-dev <cert.vuvankhoi@gmail.com>
2026-08-13 15:26:25 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -13,49 +13,24 @@ const apps = [
         descriptions: 'Link Cài Trực Tiếp V2.733 Cập Nhật Ngày 13/08/2026'
     },{
         name: '<strong><em> MENU FF IOS ✅</em></strong>',
-        url: 'https://sub2sunlock.blogspot.com/2026/05/link-tai-trong-video.html',
+        url: 'https://ipas.cc/rml3xk',
         img: 'https://i.postimg.cc/NfBCGPw3/1JSOL8E2V-AD5H4H.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 09/08/2026'
-    },{
-        name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
-        url: 'https://ipas.cc/6nec04',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 07/08/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/08/2026'
     },{
         name: '<strong><em> Minecraft PE IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1785382538',
+        url: 'https://ipas.cc/ksgo1z',
         img: 'https://i.postimg.cc/Y05W9Pv8/IMG-4773.png',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 07/08/2026'
-    },{ 
-        name: '<strong><em>Delta X VNG IOS Bản Gốc ✅</em></strong>',
-        url: 'https://sub2s.com/1785904457',
-        img: 'https://i.postimg.cc/j5Pt7VJt/IMG-3155.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.731 Cập Nhật Ngày 07/08/2026'
-    },{ 
-        name: '<strong><em>Delta X VNG IOS Fix Lag ✅</em></strong>',
-        url: 'https://sub2s.com/1785057275',
-        img: 'https://i.postimg.cc/pXQNsVQs/IMG-3417.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.731 Cập Nhật Ngày 07/08/2026'
-    },{ 
-        name: '<strong><em>Delta X Quốc Tế IOS  ✅</em></strong>',
-        url: 'https://sub2s.com/1785207240',
-        img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.731 Cập Nhật Ngày 07/08/2026'
-    },{ 
-        name: '<strong><em>Delta X Quốc Tế IOS Fix Lag  ✅</em></strong>',
-        url: 'https://sub2s.com/1785126981',
-        img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.731 Cập Nhật Ngày 07/08/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/08/2026'
     },{
-        name: '<strong><em> MENU 8 Ball Pool IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1785066381',
-        img: 'https://i.postimg.cc/Hxv3Pj2b/1783586272519-541494777965378080-584700966525922238-0b82b24f9f0cc544305843cc7a24dc0d.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 28/07/2026'
+        name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
+        url: 'https://ipas.cc/r83jfd',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 13/08/2026'
     },{
         name: '<strong><em> Proxy FF IOS ✅</em></strong>',
         url: 'https://sub2s.com/1780545960',
         img: 'https://i.postimg.cc/NfBCGPw3/1JSOL8E2V-AD5H4H.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 12/07/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/08/2026'
     },
 ];
 
